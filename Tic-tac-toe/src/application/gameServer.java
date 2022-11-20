8935d7caebbb8bc8a73332f56b559e08a303c2d8
YN <1109225291@qq.com>
2022-11-20 22:55:04 +0800

    decrese1

--- Tic-tac-toe/src/application/gameServer.java
+++ Tic-tac-toe/src/application/gameServer.java
@@ -5,25 +5,26 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class gameServer {
-    public static void main(String[] args){
-        ServerSocket serverSocket = null;
-        try {
-            serverSocket = new ServerSocket(8848);
-            Socket wait_socket = null;
-            System.out.println("Waiting player...");
-            while (true){
-                Socket come = serverSocket.accept();
-                if (wait_socket == null){
-                    wait_socket = come;
-                    System.out.println("A player enter, wait to match another player...");
-                }else {
-                    System.out.println("Match successfully, game start");
-                    new ticThread(wait_socket, come).start();
-                }
-            }
-        } catch (IOException e) {
-            System.out.println("Error!");
-            throw new RuntimeException(e);
+
+  public static void main(String[] args) {
+    ServerSocket serverSocket = null;
+    try {
+      serverSocket = new ServerSocket(8848);
+      Socket wait_socket = null;
+      System.out.println("Waiting player...");
+      while (true) {
+        Socket come = serverSocket.accept();
+        if (wait_socket == null) {
+          wait_socket = come;
+          System.out.println("A player enter, wait to match another player...");
+        } else {
+          System.out.println("Match successfully, game start");
+          new ticThread(wait_socket, come).start();
         }
+      }
+    } catch (IOException e) {
+      System.out.println("Error!");
+      throw new RuntimeException(e);
     }
+  }
 }
