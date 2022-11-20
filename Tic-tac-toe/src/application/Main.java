ef6f90b7eb788ad360112ad64b39b67c85a04970
YN <1109225291@qq.com>
2022-11-20 22:54:16 +0800

    decrese

--- Tic-tac-toe/src/application/Main.java
+++ Tic-tac-toe/src/application/Main.java
@@ -9,30 +9,31 @@ import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
 
 public class Main extends Application {
-    @Override
-    public void start(Stage primaryStage) {
-        try {
-            FXMLLoader fxmlLoader = new FXMLLoader();
 
-            fxmlLoader.setLocation(getClass().getClassLoader().getResource("mainUI.fxml"));
-            Pane root = fxmlLoader.load();
-            primaryStage.setTitle("Tic Tac Toe");
-            primaryStage.setScene(new Scene(root));
-            primaryStage.setResizable(false);
-            primaryStage.show();
-            primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
-                @Override
-                public void handle(WindowEvent event) {
-                    System.out.println("Client close");
-                    System.exit(0);
-                }
-            });
-        } catch (Exception e) {
-            e.printStackTrace();
+  @Override
+  public void start(Stage primaryStage) {
+    try {
+      FXMLLoader fxmlLoader = new FXMLLoader();
+
+      fxmlLoader.setLocation(getClass().getClassLoader().getResource("mainUI.fxml"));
+      Pane root = fxmlLoader.load();
+      primaryStage.setTitle("Tic Tac Toe");
+      primaryStage.setScene(new Scene(root));
+      primaryStage.setResizable(false);
+      primaryStage.show();
+      primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+        @Override
+        public void handle(WindowEvent event) {
+          System.out.println("Client close");
+          System.exit(0);
         }
+      });
+    } catch (Exception e) {
+      e.printStackTrace();
     }
+  }
 
-    public static void main(String[] args) {
-        launch(args);
-    }
+  public static void main(String[] args) {
+    launch(args);
+  }
 }
